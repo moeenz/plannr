d14356ee027e6ccef60e1733a48620bc5dfb8037
Moeen Zamani <moeen.zamani@gmail.com>
2018-04-18 11:45:58 +0430

    added required path and components we're going to develop to router index.js.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,11 +4,29 @@ import Router from 'vue-router'
 
 import 'buefy/lib/buefy.css'
 
+import Plans from '@/components/Plans'
+import UserIn from '@/components/UserIn'
+import UserRegister from '@/components/UserRegister'
+
 Vue.use(Buefy)
 Vue.use(Router)
 
 export default new Router({
   routes: [
-
+    {
+      path: '/user/in/',
+      name: 'user-in',
+      component: UserIn
+    },
+    {
+      path: '/user/register/',
+      name: 'user-register',
+      component: UserRegister
+    },
+    {
+      path: '/plans',
+      name: 'plans',
+      component: Plans
+    }
   ]
 })
