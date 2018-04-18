17e129ddfa87ee126bd99fc71d30265b42e8847c
Moeen Zamani <moeen.zamani@gmail.com>
2018-04-18 11:37:54 +0430

    generated frontend project boilerplate.

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app" class="container">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <h1><strong>Plannr</strong></h1>
+      </div>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/router/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Buefy from 'buefy'
+import Router from 'vue-router'
+
+import 'buefy/lib/buefy.css'
+
+Vue.use(Buefy)
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+
+  ]
+})
