f4aeb3c2dc86cce4e132eb2c4d0ab8e14fa05954
Moeen Zamani <moeen.zamani@gmail.com>
2018-04-18 12:25:23 +0430

    created UserRegister component. almost exactly like UserIn.

--- frontend/src/components/UserRegister.vue
+++ frontend/src/components/UserRegister.vue
@@ -1,12 +1,45 @@
 <template>
-  <section>
+  <section class="container">
+    <section class="columns is-mobile">
+      <section class="column">
+        <b-field label="Username">
+            <b-input v-model="username"></b-input>
+        </b-field>
+        <b-field label="Password">
+            <b-input v-model="password" type="password"></b-input>
+        </b-field>
+        <b-field>
+          <button class="button is-success" v-on:click="doRegister">Register</button>
+        </b-field>
+      </section>
+    </section>
   </section>
 </template>
 
 <script>
+import Vue from 'vue'
+
 export default {
   name: 'UserRegister',
   data () {
+    return {
+      username: undefined,
+      password: undefined
+    }
+  },
+  methods: {
+    doRegister: function () {
+      Vue.axios
+        .post(`/api/users/register/`, {
+          username: this.username,
+          password: this.password
+        })
+        .then((response) => {
+          if (response.status === 201) {
+            this.$router.push({name: 'user-in'})
+          }
+        })
+    }
   }
 }
 </script>
