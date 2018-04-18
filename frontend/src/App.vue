350fb802b90082fddca2db9ca2875311e0b64adf
Moeen Zamani <moeen.zamani@gmail.com>
2018-04-18 23:03:51 +0430

    added logout button to App navbar.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,14 +4,29 @@
       <div class="navbar-brand">
         <h1><strong>Plannr</strong></h1>
       </div>
+      <div class="navbar-end" v-show="isLoggedIn()">
+        <button class="button is-danger" v-on:click="logout">Logout</button>
+      </div>
     </nav>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  methods: {
+    logout: function () {
+      Vue.localStorage.remove('jwt_token')
+      this.$router.push({name: 'user-in'})
+    },
+    isLoggedIn: function () {
+      const jwtToken = Vue.localStorage.get('jwt_token')
+      return jwtToken !== undefined && jwtToken !== null
+    }
+  }
 }
 </script>
 
