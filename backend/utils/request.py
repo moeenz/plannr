f12cb6b13284e31544b0c2cf6a2afe78834dbeb4
Moeen Zamani <moeen.zamani@gmail.com>
2018-04-18 09:26:10 +0430

    created get_request_user helper function to retrieve requesting user object.

---
+++ backend/utils/request.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ObjectDoesNotExists
+
+User = get_user_model()
+
+
+def get_request_user(request):
+    """Returns user of the context request.
+    request can be retrieved by calling context.get('request') in
+    a serializer create method.
+    """
+
+    if request is not None and hasattr(request, 'user'):
+        try:
+            return User.objects.get(username=request.user.username)
+        except ObjectDoesNotExist:
+            raise None
+    else:
+        raise ValueError('Context request has no user attribute.')
