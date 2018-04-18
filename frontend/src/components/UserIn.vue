a9118f1efced897b3faa9904489abce30034d26f
Moeen Zamani <moeen.zamani@gmail.com>
2018-04-18 12:21:13 +0430

    created UserIn component for logging users in.

--- frontend/src/components/UserIn.vue
+++ frontend/src/components/UserIn.vue
@@ -1,12 +1,46 @@
 <template>
-  <section>
+  <section class="container">
+    <section class="columns is-mobile">
+      <section class="column">
+        <b-field label="Username">
+            <b-input v-model="username"></b-input>
+        </b-field>
+        <b-field label="Password">
+            <b-input v-model="password" type="password"></b-input>
+        </b-field>
+        <b-field>
+          <button class="button is-danger" v-on:click="doAuth">Login</button>
+        </b-field>
+      </section>
+    </section>
   </section>
 </template>
 
 <script>
+import Vue from 'vue'
+
 export default {
   name: 'UserIn',
   data () {
+    return {
+      username: undefined,
+      password: undefined
+    }
+  },
+  methods: {
+    doAuth: function () {
+      Vue.axios
+        .post(`/api/users/in/`, {
+          username: this.username,
+          password: this.password
+        })
+        .then((response) => {
+          if (response.status === 200) {
+            Vue.localStorage.set('jwt_token', response.data.token)
+            this.$router.push({name: 'plans'})
+          }
+        })
+    }
   }
 }
 </script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Buefy from 'buefy'
+import axios from 'axios'
 import Router from 'vue-router'
+import VueAxios from 'vue-axios'
 
 import 'buefy/lib/buefy.css'
 
@@ -12,6 +14,7 @@ import UserRegister from '@/components/UserRegister'
 Vue.use(Buefy)
 Vue.use(Router)
 Vue.use(VueLocalStorage)
+Vue.use(VueAxios, axios)
 
 function redirectToAuth (next) {
   const jwtToken = Vue.localStorage.get('jwt_token', undefined, String)
@@ -54,7 +57,7 @@ export default new Router({
       name: 'plans',
       component: Plans,
       beforeEnter: function (to, from, next) {
-        redirectToAuth(to, from, next)
+        redirectToAuth(next)
       }
     }
   ]
