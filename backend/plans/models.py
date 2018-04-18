eaa966be3ae958d5ce2ad4be9737c9564b51d92b
Moeen Zamani <moeen.zamani@gmail.com>
2018-04-18 09:11:32 +0430

    created Plan data model which will be hosted on cassandra.

--- backend/plans/models.py
+++ backend/plans/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+import uuid
+from cassandra.cqlengine import columns
+from django_cassandra_engine.models import DjangoCassandraModel
 
-# Create your models here.
+
+class Plan(DjangoCassandraModel):
+    """Data model to store plan objects. Each plans
+    will be owned by someone, containing a start/end datetime
+    and a description.
+    """
+
+    id = columns.UUID(primary_key=True, default=uuid.uuid4)
+    owner_id = columns.UUID(required=True)
+    start = columns.DateTime(required=True)
+    end = columns.DateTime(required=True)
+    desc = columns.Text(required=True)
+
+
+    def __str__(self):
+        return 'Plan {}'.format(self.desc)
