001a7f4a53f312952c30b5372c26b20a4d18b6ae
Moeen Zamani <moeen.zamani@gmail.com>
2018-04-18 11:46:18 +0430

    created boilerplate components for UserIn, UserRegister, Plans.

---
+++ frontend/src/components/Plans.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Plans',
+  data () {
+  }
+}
+</script>
---
+++ frontend/src/components/UserRegister.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'UserRegister',
+  data () {
+  }
+}
+</script>
