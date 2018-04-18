9ad02631cdcf151d4e98e0363b02d15ff78aaea2
Moeen Zamani <moeen.zamani@gmail.com>
2018-04-18 08:35:33 +0430

    created user_register api at /users/register/ for creating new users.

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import url
 
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
+from users.views import user_register
+
+
 urlpatterns = [
     url(r'^users/in/$', obtain_jwt_token, name='obtain_jwt_token'),
     url(r'^users/refresh/$', refresh_jwt_token, name='refresh_jwt_token'),
+    url(r'^users/register/$', user_register, name='user_register')
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from users.serializers import UserCreationSerializer
+
+
+@api_view(['POST'])
+def user_register(request):
+    """API view for registering new users.
+    """
+
+    serializer = UserCreationSerializer(data=request.data, context={'request': request})
+    if serializer.is_valid(raise_exception=True):
+        serializer.save()
+        return Response(status=status.HTTP_201_CREATED)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
