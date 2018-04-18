25d9c1022a2a52bb02576c85754e7b470ad88a21
Moeen Zamani <moeen.zamani@gmail.com>
2018-04-18 07:59:46 +0430

    created new auth.User model. replacing incremental ids with uuid field.

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db.models import UUIDField
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    """Extended user model, replacing 'id' with a uuid field
+    which is going to be used in cassandra.
+    """
+
+    id = UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
